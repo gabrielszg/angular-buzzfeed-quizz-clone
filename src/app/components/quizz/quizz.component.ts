36df906189952b33e672f81547bf5e9c55f888de
gabrielszg <gabrielsouza5@hotmail.com>
2024-09-30 11:41:17 -0300

    feat: rules of the game

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quizzQuestions from '../../data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
   title: string = '';
   questions: any;
   questionSelected: any;
@@ -16,4 +17,31 @@ export class QuizzComponent {
   questionIndex: number = 0;
   questionMaxIndex: number = 0;
   finished: boolean = false;
+  
+  ngOnInit(): void {
+    if (quizzQuestions) {
+      this.finished = false;
+      this.title = quizzQuestions.title;
+
+      this.questions = quizzQuestions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  playerChoose(value: string) {
+    this.answers.push(value);
+  }
+
+  async nextStep() {
+    this.questionIndex += 1;
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      this.finished = true;
+    }
+  }
 }
