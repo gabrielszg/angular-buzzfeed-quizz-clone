0f2e80a5752f9d7deac1888544d0678e32971112
gabrielszg <gabrielsouza5@hotmail.com>
2024-09-30 10:46:14 -0300

    feat: quizz component attributes

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './quizz.component.css'
 })
 export class QuizzComponent {
-
+  title: string = '';
+  questions: any;
+  questionSelected: any;
+  answers: string[] = [];
+  answerSelected: string = '';
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+  finished: boolean = false;
 }
